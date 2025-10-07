d68f2ce480c02d0bc71bfc9f76564b5878f9ab97
bandieragreta-netizen <bandiera.greta@gmail.com>
2025-10-07 17:38:33 +0200

    Create 01_my_first_R_script.R

---
+++ 01_my_first_R_script.R
@@ -0,0 +1,16 @@
+# This is a comment and will help humans understanding
+
+2 + 3
+
+greta <- 7 + 4
+
+greta + marco
+
+vale <- c(5, 10, 13, 20, 30)
+
+elia <- c(2, 5, 10, 40, 70)
+
+plot(elia, vale)
+
+# If I want to change the point symnol I will use pch which means point character
+plot(elia, vale, pch=2)
